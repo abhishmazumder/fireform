6707641e1beb33da1b2b5f1230bf4ae339fbc5d5
abhishek.am988@gmail.com <abhishek_mazumder>
2024-04-02 02:00:51 +0530

    Replaced firebaseConfig.js with .env and setup env vars in vercel

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "./firebaseConfig";
 import {
   getAuth,
   GoogleAuthProvider,
@@ -17,6 +16,15 @@ import {
   where,
   addDoc,
 } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
