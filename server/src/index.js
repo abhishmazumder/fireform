052e6642484d97a636631e273207386721664700
Abhishek Mazumder <abhishek.am988@gmail.com>
2025-01-25 23:57:43 +0530

    Updated turbo.json

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,6 @@
 import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config({
     path: process.env.NODE_ENV === 'production'
@@ -19,15 +14,8 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(json());
 
-const clientDistPath = path.resolve(__dirname, '../../client/dist');
-app.use(express.static(clientDistPath));
-
 app.get('/api/test', (req, res) => {
     res.json({ message: `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}` });
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(clientDistPath, 'index.html'));
 });
 
 app.listen(PORT, () => {
