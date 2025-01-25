6fd3610227c0ab68cc17435e11ebfaadacc5bfe4
Abhishek Mazumder <abhishek.am988@gmail.com>
2025-01-25 21:22:11 +0530

    Updated vercel.json to have build commands, updated vite.config.js to mention dist location

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  // optimizeDeps: {
-  //   exclude: ['js-big-decimal']
-  // }
+  build: {
+    outDir: 'dist',
+  }
 })
