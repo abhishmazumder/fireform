9a37ac242044d634368d063ef236c0695bc8d20d
Abhishek Mazumder <abhishek.am988@gmail.com>
2025-01-26 17:18:34 +0530

    Updated vite.config.js

--- apps/client/vite.config.js
+++ apps/client/vite.config.js
@@ -5,7 +5,8 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/apps/client/dist/',
   build: {
     outDir: 'dist',
-  }
+  },
 })
